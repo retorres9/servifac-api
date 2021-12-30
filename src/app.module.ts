62ab3d20523b9270e8b2c76199e2c7fd1804ce53
retorres <roberthtorres40@yahoo.es>
2021-12-29 22:02:32 -0500

    Fixing typo syncronize to synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,11 +31,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         return {
           ssl: inProduction,
           extra: {
-            ssl: { "rejectUnauthorized": false }
+            ssl: inProduction ? {rejectUnauthorized: false} : null
           },
           type: 'postgres',
           autoLoadEntities: true,
-          syncronize: true,
+          synchronize: true,
           host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
           username: configService.get('DB_USERNAME'),
